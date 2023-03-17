717f4f0ea912db45174f4fc3f5f60707519b677f
Duncan Kariuki <duncankariuki60@gmail.com>
2023-03-17 15:43:50 +0300

    build: implement storybook

---
+++ .ddddstorybook/main.js
@@ -0,0 +1,19 @@
+module.exports = {
+  stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
+  /** Expose public folder to storybook as static */
+  staticDirs: ['../public'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: '@storybook/react',
+  core: {
+    builder: {
+      name: 'webpack5',
+      options: {
+        lazyCompilation: true,
+      },
+    },
+  },
+};
